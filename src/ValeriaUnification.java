c4d5b437af3e215edba3c7120b30b398d18584d5
WhyAre <laiyr52@gmail.com>
2025-02-20 16:49:45 +0800

    Beautify output print statements

--- src/ValeriaUnification.java
+++ src/ValeriaUnification.java
@@ -43,45 +43,64 @@ public class ValeriaUnification {
         var pivotWeight = arr[pivotInd].weight();
 
         var excessWeight = leftExcess + pivotWeight;
+        System.out.printf("Excess Weight: %d\n", excessWeight);
         if (excessWeight >= partitionSize) {
             System.out.printf("Added pivot: %d\n", pivotInd);
-            output.add(arr[pivotInd].id());
+            if (pivotInd + 1 < arr.length) output.add(arr[pivotInd + 1].id());
             excessWeight -= partitionSize;
         }
 
         return findPartitions(arr, pivotInd + 1, end, partitionSize, excessWeight, output);
     }
 
-    static List<Integer> solve(Pair[] arr, int numPartitions) {
+    static void solve(Pair[] arr, int numPartitions) {
         var sumOfWeights = Arrays.stream(arr).mapToInt(Pair::weight).sum();
         var partitionSize = sumOfWeights / numPartitions;
+        System.out.printf("Partition Size: %d\n", partitionSize);
 
         var output = new ArrayList<Integer>();
 
         findPartitions(arr, 0, arr.length, partitionSize, 0, output);
 
-        return output;
+        if (output.isEmpty()) {
+            System.out.println("[0, infty)");
+            return;
+        }
+
+        System.out.printf("[0, %d), ", output.getFirst());
+        for (int i = 0; i + 1 < output.size(); i++) {
+            System.out.printf("[%d, %d), ", output.get(i), output.get(i + 1));
+        }
+        System.out.printf("[%d, infty)\n", output.getLast());
     }
 
     public static void main(String[] args) {
+//        var tc = new Pair[]{
+//                new Pair(3, 1000),
+//                new Pair(1, 2000),
+//                new Pair(2, 1000),
+//                new Pair(4, 1500),
+//                new Pair(5, 500)
+//        };
+//
+//        var tc1 = new Pair[]{
+//                new Pair(3, 150_000),
+//                new Pair(4, 42_000),
+//                new Pair(1, 1_000),
+//                new Pair(8, 151_000),
+//                new Pair(7, 109_000)
+//        };
+
         var tc = new Pair[]{
-                new Pair(3, 1000),
+                new Pair(4, 750),
                 new Pair(1, 2000),
+                new Pair(3, 250),
                 new Pair(2, 1000),
-                new Pair(4, 1500),
-                new Pair(5, 500)
+                new Pair(6, 1000),
+                new Pair(5, 2000),
+                new Pair(7, 2000)
         };
-
-        var tc1 = new Pair[]{
-                new Pair(3, 150_000),
-                new Pair(4, 42_000),
-                new Pair(1, 1_000),
-                new Pair(8, 151_000),
-                new Pair(7, 109_000)
-        };
-
-        System.out.println(solve(tc, 3));
-        System.out.println(solve(tc1, 3));
+        solve(tc, 3);
     }
 
     record Pair(int id, int weight) {
